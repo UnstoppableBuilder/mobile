add8e36f191a852853122292637cdcbdcb80c64e
13KANDER <13kander@gmail.com>
2020-10-31 16:28:39 +0700

    меню, профиль , сос

---
+++ lib/pages/home_page/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
